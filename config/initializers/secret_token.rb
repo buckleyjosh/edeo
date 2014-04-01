0cda6be5888829d92216523a2c76024073bb4b47
Josh Buckley <buckleyjosh@gmail.com>
2014-04-01 11:53:58 -0700

    created edeo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edeo::Application.config.secret_key_base = '3de2a9e7c2c332fb3dea5052828eedc2fdfff1294058185344350cecc10149235bd9bb3a9e7d7fb4255d5f15912d69ffe710bfa057e94ce05daa414820c5e170'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edeo::Application.config.session_store :cookie_store, key: '_edeo_session'
